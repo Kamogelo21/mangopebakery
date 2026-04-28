9382fc217c6fec044590404c14b7d10effa34a0b
Kamogelo Meno <karabomeno18@gmail.com>
2026-04-28 20:46:09 +0200

    Backend fixes local running, and test passing. Menu edits

--- src/main/java/com/mangope/mangopebakeryandfastfood/controller/HomeController.java
+++ src/main/java/com/mangope/mangopebakeryandfastfood/controller/HomeController.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class HomeController {
